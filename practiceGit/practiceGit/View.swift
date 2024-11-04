bf4bd03d2554022ec5f9e9100c53f084ba804f7b
minjun kim <minzzzun@gmail.com>
2024-11-05 00:15:54 +0900

    충돌

--- practiceGit/practiceGit/View.swift
+++ practiceGit/practiceGit/View.swift
@@ -8,9 +8,13 @@ class View : UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+	setUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("Error")
     }
+
+   func setUI(){
+	}		
 }
