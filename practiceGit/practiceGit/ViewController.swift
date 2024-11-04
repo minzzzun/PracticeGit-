2d32a5e6342255f7266ea94649dff0fa19bc0cf9
minjun kim <minzzzun@gmail.com>
2024-11-05 00:02:03 +0900

    파일 생성

---
+++ practiceGit/practiceGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practiceGit
+//
+//  Created by 김민준 on 11/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ practiceGit/practiceGitTests/practiceGitTests.swift
@@ -0,0 +1,17 @@
+//
+//  practiceGitTests.swift
+//  practiceGitTests
+//
+//  Created by 김민준 on 11/5/24.
+//
+
+import Testing
+@testable import practiceGit
+
+struct practiceGitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
