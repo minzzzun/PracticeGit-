cf09af1c2a498a0c49aa9a90dce8953ad9bba7fc
minjun kim <minzzzun@gmail.com>
2024-11-05 00:09:21 +0900

    메인에서 View 추가

---
+++ practiceGit/practiceGit/View.swift
@@ -0,0 +1,16 @@
+
+
+
+
+import UIKit
+
+class View : UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Error")
+    }
+}
