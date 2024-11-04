01b2ea66469e8a558f9bca79b69d72e57f679312
minjun kim <minzzzun@gmail.com>
2024-11-05 00:06:03 +0900

    View 추가

---
+++ practiceGit/practiceGit/View.swift
@@ -0,0 +1,21 @@
+//
+//  View.swift
+//  practiceGit
+//
+//  Created by 김민준 on 11/5/24.
+//
+
+import Foundation
+import UIKit
+
+
+class View : UIView {
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+}
